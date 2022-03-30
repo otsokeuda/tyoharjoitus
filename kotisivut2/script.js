931bbc03a58314e1ad5f8c3a203b742790aa2423
otsokeuda <otso.rantala@edu.keuda.fi>
2022-03-30 13:06:23 +0300

    users

--- kotisivut2/script.js
+++ kotisivut2/script.js
@@ -1,3 +1,22 @@
+var users = [
+    {
+        firstName: "Otso",
+        lastName: "Rantala"
+    },
+    {
+        firstName: "Onni",
+        lastName: "Rantala"
+    },
+    {
+        firstName: "Oona",
+        lastName: "Rantala"
+    },
+    {
+        firstName: "Fanni",
+        lastName: "Rantala"
+    }
+];
+
 function nimi() {
     document.getElementById("d1").innerHTML = "Otso Rantala";
 }
@@ -12,4 +31,11 @@ function ka() {
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     document.getElementById("d3").innerHTML = time;
+}
+
+function getUsers() {
+    var listOfUsers = users.map(user => {
+        return "<div>" + user.firstName + " " + user.lastName + "</div>"
+      });
+      document.getElementById("users").innerHTML = listOfUsers;
 }
